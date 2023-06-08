3ae098b09db9a5837e5daf5299f7fe0c38355255
Minh Nguyen <nguyenthanhminh112000@gmail.com>
2023-06-08 12:55:12 +0700

    add: support page with ssg

--- src/pages/support/index.tsx
+++ src/pages/support/index.tsx
@@ -1,4 +1,13 @@
-const Support = () => {
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const post = await res.json();
+  return {
+    props: {
+      post
+    }
+  };
+}
+const Support = ({ post }: { post: any }) => {
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-200">
@@ -7,6 +16,7 @@ const Support = () => {
       <p className="mb-6 ml-2 text-lg font-normal text-gray-500 lg:text-xl">
         This is the universal template which could fit for almost your purposes.
       </p>
+      {JSON.stringify(post)}
     </div>
   );
 };
